3291fbc98fea58da183beb412689c2b33eb2993f
VK <heyllog@gmail.com>
2026-05-14 14:12:25 +0400

    feat(appkit): delete BalanceBadge

--- packages/appkit-react/src/features/balances/index.ts
+++ packages/appkit-react/src/features/balances/index.ts
@@ -7,7 +7,6 @@
  */
 
 export * from './components/currency-item';
-export * from './components/balance-badge';
 export * from './components/send-ton-button';
 export * from './components/send-jetton-button';
 
